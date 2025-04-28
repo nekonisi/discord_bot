e6aa47c9110d42da0265f5f72269037ed3e7761e
nekonisi <kosuke.konishi@cybridge.jp>
2025-04-28 12:57:01 +0900

    ai_mother.pyの実装完了

---
+++ ai_mother.py
@@ -0,0 +1,41 @@
+from ai_assistant import AiAssistant
+from sperm import Sperm
+from dotenv import load_dotenv
+import os
+
+class AiMother:
+    GOOGLE_API_TOKEN: str
+    baby: AiAssistant
+    
+    def __init__(self):
+        """
+        - AiMotherクラスの初期化
+        - 環境変数、あるいは、dotenvファイルからGoogle APIトークンを取得して設定する
+        """
+        load_dotenv()
+        self.GOOGLE_API_TOKEN = os.getenv('GOOGLE_API_TOKEN')
+        self.baby = None
+        if self.GOOGLE_API_TOKEN is None:
+            raise ValueError("Google API Tokenが設定されていません")        
+
+    def get_pregnant(self, sperm: Sperm) -> None:
+        """
+        - AiFatherインスタンスからSpermインスタンスを受取り、妊娠する
+            - 妊娠: AiAssistantインスタンスを生成し、変数babyとして格納する
+        
+        Parameters
+        ----------
+        sperm: Sperm
+            Spermインスタンス
+        """
+        character = sperm.get_character()
+        self.baby = AiAssistant(self.GOOGLE_API_TOKEN, ai_character=character)
+
+    def bear(self) -> 'AiAssistant':
+        """
+        - 出産する
+            - 出産: AiAssistantインスタンスを返す
+        """
+        if (self.baby is None):
+            raise ValueError("妊娠していません")
+        return self.baby
