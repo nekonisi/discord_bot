d73ce7bcac358758dcd9e56d4cb179446cefc0b3
nekonisi <kosuke.konishi@cybridge.jp>
2025-04-28 13:07:58 +0900

    抽象クラスai_fatherの実装完了

---
+++ ai_father.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+from sperm import Sperm
+from ai_mother import AiMother
+
+class AiFather(ABC):
+    sperm: 'Sperm'
+
+    @abstractmethod
+    def seed(self, ai_mother: 'AiMother') -> None:
+        """
+        - AiMotherインスタンスを受取り、seedする
+        """
+        pass
+
+    @abstractmethod
+    def make_sperm(self) -> 'Sperm':
+        """
+        - Spermインスタンスから、ai_characterを生成する
+        """
+        pass
+    
